67863c1a2fdcc203e44f62cc2a26a5154a8292d9
MichaelGriffits <griffitsm@stu.otc.school.nz>
2023-08-03 19:13:00 +1200

    Options Menu WORKS!!!!
    -All features in menu can be modified
    -Bug with brightness needs to be fixed

--- Adventures of Quirrel/Assets/Code/OptionsMenuLoader.cs
+++ Adventures of Quirrel/Assets/Code/OptionsMenuLoader.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 using UnityEngine.UI;
-
+    
 public class OptionsMenuLoader : MonoBehaviour
 {
    public GameObject OpenWindow;
